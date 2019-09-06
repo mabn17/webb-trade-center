350f757ee17c3b15370c016ca8e20817cb3267a6
Martin <martin.d@live.se>
2019-09-07 00:25:51 +0200

    Added about page

--- src/Components/About/About-Page.js
+++ src/Components/About/About-Page.js
@@ -3,6 +3,7 @@ import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import Divider from '@material-ui/core/Divider';
 import { makeStyles } from '@material-ui/core/styles';
+
 import Markdown from '../../Helpers/Markdown/markdown';
 
 const useStyles = makeStyles(theme => ({
